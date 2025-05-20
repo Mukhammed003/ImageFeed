79230fa1f424954601b4aab1bfa325c2201076aa
Mukhammed <>
2025-05-20 10:50:06 +0500

    Обновлён StoryBoard main. Добавлена таблица.

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Muhammed Nurmukhanov on 19.05.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reusedIdentifier = "ImageListCell"
+}
